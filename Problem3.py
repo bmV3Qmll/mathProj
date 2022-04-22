6583af7b2256c1cd83df220d9dc16688f0ff4f64
bmV3Qmll <104069545+bmV3Qmll@users.noreply.github.com>
2022-04-22 09:58:05 +0700

    Tough one

--- Problem3.py
+++ Problem3.py
@@ -9,13 +9,16 @@ xi = 0
 yi = 3
 xf = 1
 
+# exact solution
 sol = 2+exp(-x**3)
 exact = sol.subs(x, 1).evalf()
 
+# Initialize list of coordinates
 xset = [xi]
 yset = [yi]
 
 def Euler(fxy, h, n, x0, y0):
+    # n as number of steps and h as the "width" of each step
     for i in range(n):
         fi = fxy.subs(x, x0).subs(y, y0)
         xi = x0 + h
@@ -27,14 +30,17 @@ def Euler(fxy, h, n, x0, y0):
     return y0
 
 ran = float(input("Enter step size: "))
-#Sanitize input
+# Sanitize input if necessary
+# (a)
 steps = int((xf - xi) / ran)
-print(steps)
 yf = Euler(firstDerivative, ran, steps, xi, yi)
-print(yf)
-err = abs(yf - exact) / exact * 100
-print(f"{err}%")
+print("Approximate by Euler's method: y(1) =", yf)
 
+# (c)
+err = abs(yf - exact) / exact * 100
+print(f"The corresponding error is: {err}%")
+
+# (d)
 plt.plot(xset, yset, label = "Approximation")
 x_range = np.arange(0, 1, 0.01)
 def extSol(i):
